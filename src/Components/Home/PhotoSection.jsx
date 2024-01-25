23e96bbc0e8c4b71c8c5181a790199cb90b33613
safimiazi <mohibullamiazi@gmail.com>
2024-01-25 17:43:11 +0600

    photo section images added and modify images

--- src/Components/Home/PhotoSection.jsx
+++ src/Components/Home/PhotoSection.jsx
@@ -2,13 +2,25 @@
 const PhotoSection = () => {
     return (
         <div className="grid md:grid-cols-2 grid-cols-1 gap-10 mt-32">
-            <div className="relative md:left-52  ">
-                <img className='w-64 rounded-full  absolute ' src="https://source.unsplash.com/1200x1200/?nature/?4" alt="" />
-                <img className='w-64 rounded-full  absolute  top-40 ' src="https://source.unsplash.com/1200x1200/?nature/?4" alt="" />
-                <img className='w-64 rounded-full  absolute left-40 top-20 ' src="https://source.unsplash.com/1200x1200/?nature/?4" alt="" />
-            </div>
+              <div className="relative md:left-52">
+      <img
+        className='w-64 rounded-full absolute transition-transform transform hover:scale-90 hover:z-10'
+        src="https://source.unsplash.com/1200x1200/?nature/?4"
+        alt=""
+      />
+      <img
+        className='w-64 rounded-full absolute top-40 transition-transform hover:scale-90 transform hover:z-10'
+        src="https://source.unsplash.com/1200x1200/?nature/?4"
+        alt=""
+      />
+      <img
+        className='w-64 rounded-full absolute left-40 top-20 transition-transform hover:scale-90 transform hover:z-10'
+        src="https://source.unsplash.com/1200x1200/?nature/?4"
+        alt=""
+      />
+    </div>
             <div className="md:ml-24 ">
-                <div className="card w-96 bg-blue-100 hover:shadow-xl mb-5">
+                <div className="card w-96 bg-blue-100 hover:shadow-xl mb-5 z-10 transition-transform transform hover:scale-110">
                     <div className="card-body">
                         <h2 className="card-title">Card title!</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -17,7 +29,7 @@ const PhotoSection = () => {
                         </div>
                     </div>
                 </div>
-                <div className="card w-96 bg-orange-200 hover:shadow-xl">
+                <div className="card w-96 bg-orange-200 hover:shadow-xl z-10 transition-transform transform hover:scale-110">
                     <div className="card-body">
                         <h2 className="card-title">Card title!</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
