06894779e33a15ef3488f7dd1e49019446956c28
safimiazi <mohibullamiazi@gmail.com>
2024-01-25 21:12:57 +0600

    createUserWithEmailAndPassword this function added

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,13 +1,18 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { createContext } from "react";
+import auth from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null)
 
 const AuthProvider = ({children}) => {
 
     //create user with pass and email
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
     const authInfo = {
-
+        createUser,
     }
     return (
         <AuthContext.Provider value={authInfo}>
